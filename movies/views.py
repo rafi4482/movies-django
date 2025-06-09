6c38813b8a21c45dd8e09bbcb3419f6050a16991
rafi4478 <rafi.kazla@gmail.com>
2025-06-09 20:50:48 +0600

    endif

--- movies/views.py
+++ movies/views.py
@@ -3,7 +3,10 @@ from django.shortcuts import render,HttpResponse
 # Create your views here.
 
 def index(req):
-    return render(req,'movies/index.html',{})
+    context = {
+        'movies' : ['RDR2','CSRF']
+    }
+    return render(req,'movies/index.html',context)
 
 def about(req):
     return render(req,'movies/about.html',{})
